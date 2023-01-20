41681f023207a08ba11b8b45e181f46c55a9342b
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-20 16:07:18 +0800

    style: delete some blank

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import './style/index.scss'
 export { Breadcrumbs, BreadcrumbsSeparator } from './breadcrumb'
 export type { BreadcrumbProps } from './breadcrumb'
 export { default as Button } from './Button'
-
 export { default as Layout } from './layout'
-
 export { Link, underlineClassName } from './link'
 export type { LinkProps, Underline } from './link'
 export { childrenToArray } from './shared'
-
