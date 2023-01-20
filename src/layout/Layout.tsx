4f80187fd0dac5d6e3ab3865ec3c370bee16792d
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-20 15:52:55 +0800

    feat: basic layout

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,114 @@
+import React from 'react'
+
+import classNames from 'classnames'
+
+type tagsNames = 'header' | 'footer' | 'main' | 'section'
+
+export interface GeneratorProps {
+  tagName: tagsNames
+  displayName: string
+  suffixClassnames?: string
+}
+
+export interface BasicLayoutProps extends React.HTMLAttributes<HTMLDivElement> {
+  prefixClassnames?: string
+  hasSide?: boolean
+}
+
+interface BasicPropsWithTag extends BasicLayoutProps {
+  tagName: tagsNames
+}
+
+// helper function
+export const getPrefixCls = (suffix?: string, customizePrefix?: string) => {
+  if (customizePrefix) {
+    return customizePrefix
+  }
+  return suffix ? `koo-${suffix}` : `koo`
+}
+
+function generator({ tagName, displayName, suffixClassnames }: GeneratorProps) {
+  return (BasicComponent: any) => {
+    const Adapter = React.forwardRef<HTMLElement, BasicLayoutProps>(
+      (props, ref) => {
+        const { prefixClassnames } = props
+        const prefixCls = getPrefixCls(suffixClassnames, prefixClassnames)
+        return (
+          <BasicComponent
+            ref={ref}
+            prefixClassnames={prefixCls}
+            tagName={tagName}
+            {...props}
+          />
+        )
+      }
+    )
+
+    if (process.env.NODE_ENV !== 'production') {
+      Adapter.displayName = displayName
+    }
+
+    return Adapter
+  }
+}
+
+const Basic = React.forwardRef<HTMLElement, BasicPropsWithTag>((props, ref) => {
+  const { prefixClassnames, className, tagName, children, ...other } = props
+  const classString = classNames(prefixClassnames, className)
+  Basic.displayName = tagName
+  return React.createElement(
+    tagName,
+    { className: classString, ...other, ref },
+    children
+  )
+})
+
+const BasicLayout = React.forwardRef<HTMLElement, BasicPropsWithTag>(
+  (props, ref) => {
+    const {
+      prefixClassnames,
+      className,
+      tagName: Tag,
+      children,
+      hasSide,
+      ...other
+    } = props
+    const classString = classNames(prefixClassnames, className, {
+      'koo-layout-has-side': hasSide
+    })
+    BasicLayout.displayName = Tag
+    return (
+      <Tag ref={ref} className={classString} {...other}>
+        {children}
+      </Tag>
+    )
+  }
+)
+
+const Layout = generator({
+  suffixClassnames: 'layout',
+  tagName: 'section',
+  displayName: 'Layout'
+})(BasicLayout)
+
+const Header = generator({
+  suffixClassnames: 'layout-header',
+  tagName: 'header',
+  displayName: 'Header'
+})(Basic)
+
+const Content = generator({
+  suffixClassnames: 'layout-content',
+  tagName: 'main',
+  displayName: 'Content'
+})(Basic)
+
+const Footer = generator({
+  suffixClassnames: 'layout-footer',
+  tagName: 'footer',
+  displayName: 'Footer'
+})(Basic)
+
+export { Header, Footer, Content }
+
+export default Layout
