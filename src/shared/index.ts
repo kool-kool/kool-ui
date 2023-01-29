ce0c6ac7d5f600857f480a1e990921ece0212ee9
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-29 18:40:07 +0800

    feat: encapsulate the flex detect hooks

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,2 +1,3 @@
+export { default as useFlexGapSupport } from './hooks/useFlexGapSupport'
 export { default as childrenToArray } from './utils/childrenToArray'
 export { default as getPrefixCls } from './utils/getPrefixClassName'
