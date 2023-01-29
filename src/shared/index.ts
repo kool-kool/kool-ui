df40d144dd402f483546d02b58c55e1b476debe7
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-29 13:06:00 +0800

    refactor: delete some import

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,2 @@
-export { default as useFlexGapSupport } from './hooks/useFlexGapSupport'
 export { default as childrenToArray } from './utils/childrenToArray'
 export { default as getPrefixCls } from './utils/getPrefixClassName'
