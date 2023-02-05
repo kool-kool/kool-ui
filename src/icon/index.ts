8180809d539ceeef2af93a23516084ee2b40b511
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-02-05 21:40:14 +0800

    feat: more icons

--- src/icon/index.ts
+++ src/icon/index.ts
@@ -9,6 +9,7 @@ import { ReactComponent as Check } from './svg/Check.svg'
 import { ReactComponent as Close } from './svg/Close.svg'
 import { ReactComponent as Exclamation } from './svg/Exclamation.svg'
 import { ReactComponent as Home } from './svg/Home.svg'
+import { ReactComponent as Info } from './svg/Info.svg'
 import { ReactComponent as List } from './svg/List.svg'
 import { ReactComponent as MoreHoriz } from './svg/MoreHoriz.svg'
 import { ReactComponent as RoundKeyboardArrowRight } from './svg/RoundKeyboardArrowRight.svg'
@@ -24,3 +25,4 @@ export const ArrowUpIcon = IconWrapper(ArrowUp)
 export const CheckIcon = IconWrapper(Check)
 export const CloseIcon = IconWrapper(Close)
 export const ExclamationIcon = IconWrapper(Exclamation)
+export const InfoIcon = IconWrapper(Info)
