0444e97c275c3004479e24010426f5b6b91cd638
linhaishe <42030043+linhaishe@users.noreply.github.com>
2023-02-14 23:21:51 +0800

    feat: finished message component basic function

--- src/icon/index.ts
+++ src/icon/index.ts
@@ -7,6 +7,7 @@ import { ReactComponent as ArrowRight } from './svg/ArrowRight.svg'
 import { ReactComponent as ArrowUp } from './svg/ArrowUp.svg'
 import { ReactComponent as Check } from './svg/Check.svg'
 import { ReactComponent as Close } from './svg/Close.svg'
+import { ReactComponent as Error } from './svg/Error.svg'
 import { ReactComponent as Exclamation } from './svg/Exclamation.svg'
 import { ReactComponent as Home } from './svg/Home.svg'
 import { ReactComponent as Info } from './svg/Info.svg'
@@ -14,6 +15,8 @@ import { ReactComponent as List } from './svg/List.svg'
 import { ReactComponent as Loading } from './svg/Loading.svg'
 import { ReactComponent as MoreHoriz } from './svg/MoreHoriz.svg'
 import { ReactComponent as RoundKeyboardArrowRight } from './svg/RoundKeyboardArrowRight.svg'
+import { ReactComponent as Success } from './svg/Success.svg'
+import { ReactComponent as Warn } from './svg/Warning.svg'
 
 export const MoreHorizIcon = IconWrapper(MoreHoriz)
 export const HomeIcon = IconWrapper(Home)
@@ -28,3 +31,6 @@ export const CloseIcon = IconWrapper(Close)
 export const ExclamationIcon = IconWrapper(Exclamation)
 export const InfoIcon = IconWrapper(Info)
 export const LoadingIcon = IconWrapper(Loading)
+export const WarningIcon = IconWrapper(Warn)
+export const SuccessIcon = IconWrapper(Success)
+export const ErrorIcon = IconWrapper(Error)
