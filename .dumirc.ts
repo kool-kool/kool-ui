2ed1e2001411ec54d46056dda6f46ed75b903d71
yixiaojiu <1918418506@qq.com>
2023-02-20 22:31:45 +0800

    build: :memo: doc build workflow

---
+++ .dumirc.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'dumi'
+
+export default defineConfig({
+  title: 'kool-ui', // 站点名称
+  outputPath: 'doc-site', // 输出文件夹
+  base: '/kool-ui',
+  publicPath: '/kool-ui/'
+})
